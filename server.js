6f6adfb365652ecc7b2ea3863ea568b3cfe43720
davidbelleza <davidbelleza@gmail.com>
2020-07-28 16:37:37 -0500

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {animals} = require('./data/animals')
 
 // instantiate server
-const PORT = process.nextTick.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
