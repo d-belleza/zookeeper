da3675d5569f0c65ea09cac65e8be11d6e197bbf
davidbelleza <davidbelleza@gmail.com>
2020-07-29 19:52:02 -0500

    add front end

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const {animals} = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -76,6 +77,21 @@ function validateAnimal(animal) {
   return true;
 }
 
+// load HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+
+// log to console of new server
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
